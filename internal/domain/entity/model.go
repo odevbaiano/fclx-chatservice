293dc7a6267b1584f8abc6ba38a30ad2c0265a91
Altanir <altanir@odevbaiano.com.br>
2023-04-17 22:48:48 -0300

    feat(domain): :sparkles: add entities domain

---
+++ internal/domain/entity/model.go
@@ -0,0 +1,20 @@
+package entity
+
+type Model struct {
+	Name      string
+	MaxTokens int
+}
+
+func NewModel(name string, maxTokens int) *Model {
+	return &Model{
+		Name:      name,
+		MaxTokens: maxTokens,
+	}
+}
+
+func (m *Model) GetMaxTokens() int {
+	return m.MaxTokens
+}
+func (m *Model) GetName() string {
+	return m.Name
+}
